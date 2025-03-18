8a069a116f68c6d2cfafe5629718b8db4a91e518
Yash <67926590+Yash094@users.noreply.github.com>
2025-03-18 20:23:50 +0530

    repro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,53 @@
 "use client";
-
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
+import {prepareTransaction, toWei } from "thirdweb";
 import { client } from "./client";
+import {
+  ConnectButton,
+  TransactionButton,
+} from "thirdweb/react";
+
+import { sepolia } from "thirdweb/chains";
 
 export default function Home() {
+ 
+
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
+    <main className="min-h-screen p-8 flex flex-col items-center gap-6">
+      <h1 className="text-2xl font-bold mb-4">Wallet Connection</h1>
 
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
-        </div>
+      <div className="flex flex-col items-center gap-4">
+        <ConnectButton
+          client={client}
+          accountAbstraction={{
+            chain: sepolia,
+            sponsorGas: true
+          }}
 
-        <ThirdwebResources />
+        />
+        
+        <TransactionButton
+          transaction={() => {
+            // Create a transaction object and return it
+            const transaction = prepareTransaction({
+              to: "0x65CC52Fb427928F22964Aec6b217b80CfF382EAC",
+              chain: sepolia,
+              client: client,
+              value: toWei("0.0"),
+            });
+            return transaction;
+          }}
+          onError={(error) => {
+            // Handle error
+            console.error(error);
+          }}
+
+
+        >
+          Confirm Transaction
+        </TransactionButton>
       </div>
     </main>
   );
 }
 
-function Header() {
-  return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
 
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
-      </p>
-    </header>
-  );
-}
-
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
-  );
-}
